64b9861e33de81c8721fd7e7a6da093a175ecede
leonardo Krindges <krindgesleonardo3@gmail.com>
2023-11-14 21:11:20 -0300

    add: readme, refatorado todas as rotas

---
+++ README.md
@@ -0,0 +1,114 @@
+## Instalação
+
+Clonar o repositório
+
+```bash
+  git clone [https://github.com/](https://github.com/Leokrindges/API_lista_de_recados)
+```
+
+Instalar as depêndencias do projeto
+
+```bash
+  yarn install | npm install
+```
+
+Iniciar o projeto
+
+```bash
+  yarn run dev | yarn dev | npm run dev
+```
+
+## Documentação da API
+
+Esta API foi desenvolvida para gerenciar informações sobre recados. Ela oferece funcionalidades para criar, ler, atualizar e excluir recados.
+Além de criar e fazer o login de usuários.
+
+
+#### Retorna uma lista de todos os usuários e seus recados cadastrados.
+
+```http
+  GET /usuario
+```
+
+
+#### Retorna todos os detalhes de um usuario e recado específico com base no ID fornecido na URL.
+
+```http
+  GET /usuario/recados/:id
+```
+
+| Parâmetro   | Tipo       | Descrição                           |
+| :---------- | :--------- | :---------------------------------- |
+| `id` | `number` | **Opcional**. id do usuario para filtragem
+
+
+
+
+#### Criar um recado
+
+```http
+  POST /usuario/recado/:id
+```
+
+| Parâmetro   | Tipo               | Descrição                                   |
+| :---------- | :----------------- | :------------------------------------------ |
+| `id`        | `string`           | **Obrigatório**. id do usuario |
+| `Recados`   | `Array de objetos` | **Obrigatório**. lista de com recados |
+| `Titulo`       | `String`           | **Obrigatório**. titulo do recado |
+| `Descrição `      | `string`           | **Obrigatório**. descrição do recado |
+
+
+
+
+#### Atualiza recado existente com base no ID do usuario e ID do recado fornecido na URL.
+
+```http
+  PUT /usuario/recado/:idUsuario/:idRecado
+```
+
+| Parâmetro   | Tipo               | Descrição                                   |
+| :---------- | :----------------- | :------------------------------------------ |
+| `Recados`   | `Array de objetos` | **Obrigatório**. lista de com recados |
+| `Titulo`       | `String`           | **Obrigatório**. titulo do recado |
+| `Descrição `      | `string`           | **Obrigatório**. descrição do recado |
+
+
+
+### Exclui um recado com base no ID do usuario e ID do recado fornecido na URL.
+```http
+  DELETE /usuario/recado/:idUsuario/:idRecado
+```
+
+| Parâmetro   | Tipo       | Descrição                                   |
+| :---------- | :--------- | :------------------------------------------ |
+| `idUsuario`      | `String` | **Obrigatório**. id do usuario |
+| `idRecado`      | `String` | **Obrigatório**. id do recado |
+
+
+
+### Cria um usuario
+```http
+  POST /usuario
+```
+
+| Parâmetro   | Tipo       | Descrição                                   |
+| :---------- | :--------- | :------------------------------------------ |
+| `ID`      | `String` | **Obrigatório**. id do usuario |
+| `Nome`      | `String` | **Obrigatório**. Nome do usuário |
+| `E-mail`      | `String` | **Obrigatório**. E-mail do usuário |
+| `Senha`      | `String` | **Obrigatório**. Senha do usuário |
+
+
+### Login
+```http
+  POST /usuario/login
+```
+
+| Parâmetro   | Tipo       | Descrição                                   |
+| :---------- | :--------- | :------------------------------------------ |
+| `E-mail`      | `String` | **Obrigatório**. E-mail do usuário |
+| `Senha`      | `String` | **Obrigatório**. Senha do usuário |
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { randomUUID } from 'node:crypto';
 
 //pega bliblioteca do 'express' e importa a funcionalidade chamada express
 import express, { request, response } from 'express';
@@ -9,8 +10,6 @@ import bcrypt from "bcrypt";
 //biblioteca para criar token e deixar o usuario logado mais tempo
 import jwt from "jsonwebtoken";
 
-
-
 //o express usado acima é para criar uma aplcação, criar um servidor no back-end
 const app = express();
 
@@ -19,45 +18,61 @@ app.use(express.json());
 
 let verifyJwt = function (req, res, next) {
     const body = req.body;
-  
+
     jwt.verify(body.accessToken, "growdev", (err, user) => {
-     
-      if (err) {
-        return res.status(403).json("Access token invalido");
-      }
-  
-      req.user = user;
-  
-      next();
+
+        if (err) {
+            return res.status(403).json("Access token invalido");
+        }
+
+        req.user = user;
+
+        next();
     });
-  };
+};
 
-
-let geraIdUsuario = 1
-const listaRecados = []
 const usuarios = [
     {
-        id: geraIdUsuario++,
+        id: randomUUID(),
         nome: "Leonardo Krindges",
         email: "leonardo@mail.com",
-        senha: "$2a$06$oWaGUzjgm8wGpV8otyteyuuiLM3blA6ul2q.X3X6df33zLStZBwXK"
+        senha: "$2a$06$oWaGUzjgm8wGpV8otyteyuuiLM3blA6ul2q.X3X6df33zLStZBwXK",
+        recados: [
+            {
+                id: randomUUID(),
+                titulo: "Passeio bob",
+                descricao: "Levar no parque"
+            }
+        ]
     },
     {
-        id: geraIdUsuario++,
+        id: randomUUID(),
         nome: "Jéssica Stein",
         email: "jessica@mail.com",
-        senha: "$2a$06$6aRs1GjmDyjTFUPzJWqx7OohIj74m4KNOhMoCE9LjLp6e/.BZgJOe"
+        senha: "$2a$06$6aRs1GjmDyjTFUPzJWqx7OohIj74m4KNOhMoCE9LjLp6e/.BZgJOe",
+        recados: [
+            {
+                id: randomUUID(),
+                titulo: "Tomar remedio",
+                descricao: "Tomar as 12h"
+            }
+        ]
     },
     {
-        id: geraIdUsuario++,
+        id: randomUUID(),
         nome: "Silvania Souza",
         email: "Silvania@mail.com",
-        senha: "$2a$06$fyo17xUNtk0eHJt4ehjdku6a7DQpn.HChRhQfneBHMdH9XbvW75ha"
+        senha: "$2a$06$fyo17xUNtk0eHJt4ehjdku6a7DQpn.HChRhQfneBHMdH9XbvW75ha",
+        recados: [
+            {
+                id: randomUUID(),
+                titulo: "Mercado",
+                descricao: "Comprar pão"
+            }
+        ]
     }
 
 ]
-let geraIdRecado = 1
-
 
 //configuro a rota; '/' é a mesma coisa que http://api.com, é como se fosse a toda principal da api
 //request contem informações da requisição que o front-end faz pro back-end
@@ -66,20 +81,42 @@ app.get('/', (request, response) => {
     return response.json('OK');
 });
 
-//EXIBE LISTA DE RECADOS
-app.get('/listaRecados', (request, response) => {
-    return response.json(listaRecados)
+//EXIBE USUARIOS E SEUS RECADOS
+app.get('/usuario', (request, response) => {
+    return response.json(usuarios)
 })
 
-//CRIA LISTA DE RECADOS
-app.post('/listaRecados', (request, response) => {
-    const body = request.body
+//BUSCA USUARIOS E SEUS RECADOS PELO ID
+app.get('/usuario/recados/:id', (request, response) => {
+    const params = request.params
 
-    const recado = {
-        id: geraIdRecado++,
-        titulo: body.titulo,
-        descricao: body.descricao
+
+    const pegaUsuariosPeloIndice = usuarios.findIndex((usuario) => {
+        return usuario.id == params.id
+    })
+
+    if(pegaUsuariosPeloIndice === -1) {
+        return response.status(400).json("Usuario não encontrado")
     }
+    
+    return response.json(usuarios[pegaUsuariosPeloIndice]);
+
+})
+
+
+//CRIAR RECADO
+app.post('/usuario/recado/:id', (request, response) => {
+    const body = request.body
+    const params = request.params.id
+
+    const pegaIndice = usuarios.findIndex(usuario => {
+        return params === usuario.id
+    })
+
+    if (pegaIndice == -1) {
+        return response.status(400).json("Usuario não encontrado")
+    }
+
     if (body.titulo == undefined) {
         return response.status(400).json("Titulo não informado!")
     }
@@ -88,67 +125,79 @@ app.post('/listaRecados', (request, response) => {
         return response.status(400).json("Descrição não informada!")
     }
 
+    const recado = {
+        idRecado: randomUUID(),
+        titulo: body.titulo,
+        descricao: body.descricao
+    }
 
-    listaRecados.push(recado)
-    console.log(body);
-    return response.json("Recado cadastrado com sucesso!!")
+    usuarios[pegaIndice].recados.push(recado)
+
+    return response.status(201).json("Recado criado com sucesso")
 })
 
-//ATUALIZA LISTA DE RECADOS
-app.put('/listaRecados/:id', (request, response) => {
+//ATUALIZA RECADOS
+app.put('/usuario/recado/:idUsuario/:idRecado', (request, response) => {
     const body = request.body
-    const params = request.params
+    const idUsuario = request.params.idUsuario
+    const idRecado = request.params.idRecado
 
-    const alteraIndiceListaRecados = listaRecados.findIndex((recado) => {
-        return recado.id === Number(params.id)
+    const pegaIndiceUsuario = usuarios.findIndex(usuario => {
+        return usuario.id == idUsuario
+    })
+    console.log(pegaIndiceUsuario);
+    if (pegaIndiceUsuario === -1) {
+        return response.status(400).json("Usuário inválido")
+    }
+
+    const pegaIndiceRecado = usuarios[pegaIndiceUsuario].recados.findIndex(recado => {
+        return recado.id === idRecado
     })
 
-    if (alteraIndiceListaRecados === -1) {
-        return response.status(400).json("Recado não encontrado")
-    } else {
-        for (let i = 0; i < listaRecados.length; i++) {
-            if (listaRecados[i].id == Number(params.id)) {
-                listaRecados[i].titulo = body.titulo
-                listaRecados[i].descricao = body.descricao
-
-                return response.json("Alterado com sucesso!!")
-            }
-        }
+    if (pegaIndiceRecado === -1) {
+        return response.status(400).json("Recado inválido")
     }
+
+    const recado = {
+        titulo: body.titulo,
+        descricao: body.descricao
+    }
+
+    usuarios[pegaIndiceUsuario].recados[pegaIndiceRecado] = recado
+
+    return response.status(201).json("Recado atualizado com sucesso!!")
 
 })
 
 //DELETA RECADO
-app.delete('/listaRecados/:id', verifyJwt, (request, response) => {
-    const params = request.params
+app.delete('/usuario/recado/:idUsuario/:idRecado', (request, response) => {
+    const body = request.body
+    const idUsuario = request.params.idUsuario
+    const idRecado = request.params.idRecado
 
-    const apagaIndiceListaRecados = listaRecados.findIndex((recado) => {
-        console.log(recado);
-        return recado.id === Number(params.id)
+    const pegaIndiceUsuario = usuarios.findIndex(usuario => {
+        return usuario.id == idUsuario
     })
-    if (apagaIndiceListaRecados == -1) {
-        return response.status(400).json("Recado não encontrado!")
+    console.log(pegaIndiceUsuario);
+    if (pegaIndiceUsuario === -1) {
+        return response.status(400).json("Usuário inválido")
     }
 
-    console.log(apagaIndiceListaRecados);
+    const pegaIndiceRecado = usuarios[pegaIndiceUsuario].recados.findIndex(recado => {
+        return recado.id === idRecado
+    })
 
-    delete listaRecados[apagaIndiceListaRecados]
-
-    return response.json("Recado apagado com sucesso!")
-})
-
-
-//BUSCA USUARIOS
-app.get('/usuario:nome?', (request, response) => {
-    const queryParams = request.query
-    if (queryParams.nome != undefined) {
-        const nomeDosUsuarios = usuarios.filter((usuario) => {
-            return usuario.nome == queryParams.nome
-        })
-        return response.json(nomeDosUsuarios)
-    } else {
-        return response.json(usuarios);
+    if (pegaIndiceRecado === -1) {
+        return response.status(400).json("Recado inválido")
     }
+
+    const recado = {
+        titulo: body.titulo,
+        descricao: body.descricao
+    }
+
+    delete usuarios[pegaIndiceUsuario].recados[pegaIndiceRecado]
+    return response.json("Recado deletado com sucesso!!")
 })
 
 
@@ -173,7 +222,7 @@ app.post('/usuario', async (request, response) => {
         return usuario.email === body.email
     })
 
-    if(existeEmail != undefined) {
+    if (existeEmail != undefined) {
         return response.json("E-mail já cadastrado!!")
     }
 
@@ -182,7 +231,7 @@ app.post('/usuario', async (request, response) => {
     const hashedSenha = await bcrypt.hash(body.senha, 6)
 
     const usuario = {
-        id: geraIdUsuario++,
+        id: randomUUID(),
         nome: body.nome,
         email: body.email,
         senha: hashedSenha
@@ -192,6 +241,8 @@ app.post('/usuario', async (request, response) => {
     return response.json("Usuario cadastrado com sucesso!!")
 })
 
+
+//LOGIN USUARIO
 app.post('/usuario/login', async (request, response) => {
     const body = request.body
 
@@ -219,55 +270,12 @@ app.post('/usuario/login', async (request, response) => {
     }
 
     const accessToken = jwt.sign({ username: existeEmail.nome },
-        "growdev", {expiresIn: "1800s",}
+        "growdev", { expiresIn: "1800s", }
     );
     //caso de tudo certo na validacão envia o token
-    return response.status(201).json({accessToken,
+    return response.status(201).json({
+        accessToken,
     });
-})
-
-
-//ATUALIZA USUARIO
-app.put('/usuario/:id', (request, response) => {
-    const params = request.params
-    const body = request.body
-
-    const alteraIndiceUsuario = usuarios.findIndex((usuario) => {
-        return usuario.id === Number(params.id)
-    })
-
-    if (alteraIndiceUsuario === -1) {
-        return response.json("Usuario não encontrado")
-    }
-
-    const usuario = {
-        nome: body.nome,
-        email: body.email,
-        senha: body.senha
-    }
-
-    usuarios[alteraIndiceUsuario] = usuario
-
-    return response.json("Usuario atualizado com sucesso!")
-
-})
-
-
-//DELETA USUARIO
-app.delete('/usuario/:id', verifyJwt, (request, response) => {
-    const params = request.params
-
-    const apagaIndiceUsuario = usuarios.findIndex((usuario) => {
-        return usuario.id === Number(params.id)
-    })
-
-    if(apagaIndiceUsuario === -1) {
-        return response.status(400).json("ID inválido")
-    }
-
-    delete usuarios[apagaIndiceUsuario]
-
-    return response.json("Usuario apagado com sucesso!")
 })
 
 //listem quer dizer o que servidor vai estar ouvindo requisições, esperando, na porta 8080,
