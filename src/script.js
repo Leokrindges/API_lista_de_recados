8c1b82cc50dd4059800014802b453772358f4f68
leonardo Krindges <krindgesleonardo3@gmail.com>
2024-01-23 23:55:58 -0300

    refatorado login e add alerts bootstrap

--- src/script.js
+++ src/script.js
@@ -1,38 +1,54 @@
 
-const containerlogin = document.getElementById('loginInvalido')
-containerlogin.innerHTML = ""
-containerlogin.setAttribute('style', 'color:red; padding-left: 80px; margin-top:8px; font-size: 20px;')
-
-
 const instance = axios.create({
     baseURL: 'http://localhost:8080',
 });
 
+const capturaDados = document.getElementById("login")
+capturaDados.addEventListener('submit', async (evento) => {
+    evento.preventDefault()
 
-async function login(event) {
-    event.preventDefault()
+    const email = evento.target.email.value
+    const senha = evento.target.senha.value
 
-    const email = event.srcElement.email.value
-    const senha = event.srcElement.senha.value
+    await login(email, senha)
 
+})
+
+async function login(email, senha) {
     try {
-        console.log(event);
         const resposta = await instance.post(`/usuario/login`, {
             email,
             senha
         })
-        console.log(resposta);
 
         accessToken = resposta.data.dadosUsuarios.accessToken
         nome = resposta.data.dadosUsuarios.nome
-       
+
         localStorage.setItem("access_token", accessToken)
         localStorage.setItem("nome", nome)
 
+
         window.location.href = "http://127.0.0.1:5500/home.html"
+        return true;
 
     } catch (error) {
-        containerlogin.innerHTML = "Credenciais inválidas!!"
+        criarAlerta("Crendeciais inválidas!")
         console.log(error);
+        return false;
     }
+}
+
+const containerFeedbck = document.getElementById('container-feedback')
+
+function criarAlerta(mensagem, tipo) {
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <div class="alert alert-${tipo} alert-danger alert-dismissible" role="alert">
+       <div>${mensagem}</div>
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    `;
+
+    containerFeedbck.append(div)
+
 }
